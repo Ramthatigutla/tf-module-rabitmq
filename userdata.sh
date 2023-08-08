6443ea21ca3de3f123bfbc93094aa789706724c0
ram <ramthatigutla@gmail.com>
2023-08-08 14:20:17 +0530

    ngw

--- userdata.sh
+++ userdata.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 labauto ansible
-ansible-pull -i localhost, -U https://github.com/Ramthatigutla/roboshop-ansible main.yml -e role_name=rabitmq -e env=${env} &>>/opt/ansible.log\
+ansible-pull -i localhost, -U https://github.com/Ramthatigutla/roboshop-ansible main.yml -e role_name=rabbitmq -e env=${env} &>>/opt/ansible.log\
